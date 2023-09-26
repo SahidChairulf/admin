5c3788ffcebe04ed8adbc3a72b896699f3d2932e
SahidChairulf <sahidchairulf@gmail.com>
2023-09-26 21:27:38 +0800

    admin

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+</script>
+<template>
+  <div class="dark:bg-white">
+    <Sidebar></Sidebar>
+  <div name="slide-fade" mode="out-in">
+      <router-view/> 
+  </div>
+  </div>
+</template>
+<style>
+  .slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+  }
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateX(2em);
+    opacity: 0;
+  }
+</style>
